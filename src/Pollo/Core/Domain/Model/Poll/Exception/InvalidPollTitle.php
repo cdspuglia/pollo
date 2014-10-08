bafc45c443faa68f54bdd3d3e77808662efd3b19
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2014-10-08 20:55:06 +0000

    Scrutinizer Auto-Fixes
    This patch was automatically generated as part of the following inspection:
    https://scrutinizer-ci.com/g/cdspuglia/pollo/inspections/d5bbac8f-0310-4e02-9224-80997420d163
    
    Enabled analysis tools:
     - PHP Analyzer
     - PHP Mess Detector
     - PHP Code Sniffer
     - Security Advisory Checker
     - PHP Copy/Paste Detector
     - PHP Lines Of Code
     - PHP PDepend
     - External Code Coverage
     - PHP Change Tracking Analyzer

--- src/Pollo/Core/Domain/Model/Poll/Exception/InvalidPollTitle.php
+++ src/Pollo/Core/Domain/Model/Poll/Exception/InvalidPollTitle.php
@@ -4,6 +4,9 @@ namespace Pollo\Core\Domain\Model\Poll\Exception;
 
 final class InvalidPollTitle extends \Exception
 {
+    /**
+     * @param string $poll_title
+     */
     public function __construct($poll_title)
     {
         $this->message = "Invalid poll title '$poll_title'";
