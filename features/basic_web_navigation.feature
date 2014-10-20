314ebe409ec7ba53f35b223f9ebd2ba354d47631
Nicolò Pignatelli <pignatelli.nicolo@gmail.com>
2014-10-20 23:03:59 +0200

    Introduced Behat w/ goutte and selenium2 drivers

---
+++ features/basic_web_navigation.feature
@@ -0,0 +1,8 @@
+Feature: Navigate to Pollo web application
+  In order to use Pollo from its web interface
+  As a user
+  I want to access Pollo web application
+
+  Scenario: User can navigate to homepage
+    Given I am on the homepage
+    Then I should see "Pollo homepage"
--- src/Pollo/Core/Domain/Repository/PollRepository.php
+++ src/Pollo/Core/Domain/Repository/PollRepository.php
@@ -3,6 +3,7 @@
 namespace Pollo\Core\Domain\Repository;
 
 use Broadway\EventHandling\EventBusInterface;
+use Broadway\EventSourcing\AggregateFactory\PublicConstructorAggregateFactory;
 use Broadway\EventSourcing\EventSourcingRepository;
 use Broadway\EventStore\EventStoreInterface;
 
@@ -10,6 +11,11 @@ final class PollRepository extends EventSourcingRepository
 {
     public function __construct(EventStoreInterface $eventStore, EventBusInterface $eventBus)
     {
-        parent::__construct($eventStore, $eventBus, 'Pollo\Core\Domain\Model\Poll\Poll');
+        parent::__construct(
+            $eventStore,
+            $eventBus,
+            'Pollo\Core\Domain\Model\Poll\Poll',
+            new PublicConstructorAggregateFactory()
+        );
     }
 }
