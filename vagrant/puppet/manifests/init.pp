57ebbc8d25f0a0ad235d5876968ccb49891342f7
Nicolò Pignatelli <pignatelli.nicolo@gmail.com>
2014-11-21 08:06:13 +0100

    Introduced vagrant VM for development

---
+++ vagrant/puppet/manifests/init.pp
@@ -0,0 +1,20 @@
+exec { 'apt-get update':
+  path => '/usr/bin',
+}
+
+file { '/pollo/':
+  ensure => 'directory',
+}
+
+file { '/etc/hosts':
+  ensure => present,
+}
+
+exec { 'add-pollo-host':
+  unless => "grep pollo /etc/hosts",
+  user => root,
+  command => 'echo "127.0.0.1 pollo.local" >> /etc/hosts',
+  path => ['/bin'];
+}
+
+include nginx, php, eventstore
