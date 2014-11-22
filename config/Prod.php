c66b5610bb5a14fcb70b84ee8c3612f481eefa50
Giuseppe Santoro <giuseppesantoro87@gmail.com>
2014-11-22 13:03:22 +0100

    Introduced bower, bootstrap-material-desig and designed the page layout

--- config/Prod.php
+++ config/Prod.php
@@ -9,7 +9,14 @@ class Prod extends Config
 {
     public function define(Container $di)
     {
-
+        // Template engine
+        $di->set('pollo/web:templating', $di->lazyNew('Pollo\Web\Templating\TwigTemplateEngine', array(
+            'loader' => $di->lazyNew(
+                    '\Twig_Loader_Filesystem',
+                    array(__DIR__ . '/../src/Pollo/Web/Resources/templates')
+                ),
+            'options' => array('cache' => __DIR__ . '/../tmp/cache/twig')
+        )));
     }
 
     public function modify(Container $di)
